34c3d74d49411110efcd1c2be7178072c1624a9f
Gintile Daugelaite <daugelaiteg@gmail.com>
2023-01-30 17:58:05 +0200

    Initial HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import "./App.scss";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import HomePage from "./components/HomePage";
+import QuizPage from "./components/QuizPage";
 
-import './App.scss';
-
-function App() {
-  return (
-<h1>ha</h1>
-  );
+const App = () => {
+return(
+<Router>
+  <Routes>
+  <Route exact path="/" element={< HomePage />}/>
+  <Route exact path="/quiz" element={< QuizPage />} />
+  </Routes>
+</Router>
+)
 }
 
 export default App;
