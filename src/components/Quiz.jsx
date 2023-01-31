f35506d40de33829378c85df231a8090af3b2ce3
Gintile Daugelaite <daugelaiteg@gmail.com>
2023-01-31 14:10:02 +0200

    Fetch data and shuffle possible answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-const Quiz = () => {
+const Quiz = ({question, correct_answer, incorrect_answers}) => {
+    const [shuffledAnswers, setShuffledAnswers] = useState([])
+
+    const allAnswers = incorrect_answers.concat(correct_answer)
+  
+    
+  useEffect(() => {
+    setShuffledAnswers(shuffleArray(allAnswers));
+  }, []);
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
   return (
+
+
     <div className="quiz">
-      <h2 className="quiz__question">How would one say goodbye in Spanish?</h2>
+      <h2 className="quiz__question">{question.replaceAll("&quot;", "\"").replaceAll("&amp;", "&").replaceAll("&#039;s", "'s")}</h2>
       <ul className="quiz__answer-container">
-        <li className="quiz__answer">Adios</li>
-        <li className="quiz__answer">Hola</li>
-        <li className="quiz__answer">Bye</li>
-        <li className="quiz__answer">Salir</li>
+        {shuffledAnswers.map((answer, i) => (<li className="quiz__answer" key={i}>{answer}</li>))}
+        {/* <li className="quiz__answer">{correct_answer}</li> */}
+        {/* {incorrect_answers.map((wrong, i) => <li className="quiz__answer" key={i}>{wrong.replaceAll("&#039;s", "'s").replaceAll("&auml;", "ä").replaceAll("&aring;", "å").replaceAll(/&#039;/g, "'")}</li>)} */}
+{/*         
+        <li className="quiz__answer">{incorrect_answers[0]}</li>
+        <li className="quiz__answer">{incorrect_answers[1]}</li>
+        <li className="quiz__answer">{incorrect_answers[2]}</li> */}
       </ul>
     </div>
   );
--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import Quiz from "./Quiz";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const QuizPage = () => {
-  return(
+  const [quizQuestions, setQuizQuestions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
+      )
+      .then((res) => {
+        setQuizQuestions(res.data.results);
+        console.log(res.data.results)
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+
+
+  return (
     <section className="quiz-page-container">
-    <Quiz />
+    
+      {quizQuestions.map((quizQuestion, i) => <Quiz key={i} {...quizQuestion}/>)}
     </section>
-  )
+  );
 };
 
 export default QuizPage;
